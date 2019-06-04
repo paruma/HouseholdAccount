93ee47e08009db9a8ff8155843f88d0a21af07d1
paruma <48xi3dparu@i.softbank.jp>
2019-06-04 20:06:36 +0900

    DBの初期値を変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,8 +10,9 @@ create table household_account (
 ;
 create sequence id_seq start with 1000;
 
-insert into household_account (id, name, price, date) values (1,'本', 1000, '1900-1-1');
-insert into household_account (id, name, price, date) values (2,'食品', 350, '1900-1-2');
+insert into household_account (id, name, price, date) values (1,'本', 1000, '2017-2-3');
+insert into household_account (id, name, price, date) values (2,'野菜', 350, '2019-6-3');
+insert into household_account (id, name, price, date) values (3,'お肉', 500, '2019-6-4');
 
 # --- !Downs
 drop table if exists household_account;
